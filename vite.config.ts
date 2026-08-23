be5969feb33730233b1c9322083aac1170705031
absurD-666 <shvayte@gmail.com>
2026-08-23 13:55:52 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+import { vlyPlugin } from "@vly-ai/integrations";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [vlyPlugin(), react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
